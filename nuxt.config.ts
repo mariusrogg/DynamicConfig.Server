809c3adfc08b7852c9e1cee04ba623633e0b5497
Marius Roggenbuck <roggenbuckmarius@gmail.com>
2024-02-22 22:40:24 +0100

    Added primevue and vitest ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxt/test-utils/module'
-  ]
+    '@nuxt/test-utils/module',
+    'nuxt-primevue'
+  ],
+  css: ['primevue/resources/themes/aura-dark-green/theme.css']
 })
