429817caf5021f86311a25cae42e4af0e6bbac70
Marius Roggenbuck <roggenbuckmarius@gmail.com>
2024-02-25 12:23:58 +0100

    Added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxt/test-utils/module',
-    'nuxt-primevue'
+    'nuxt-primevue',
+    '@pinia/nuxt'
   ],
   css: ['primevue/resources/themes/aura-dark-green/theme.css']
 })
