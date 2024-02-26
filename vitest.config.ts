12c12dc5052198e77484370595e682ae1adee7cd
Marius Roggenbuck <roggenbuckmarius@gmail.com>
2024-02-26 21:08:04 +0100

    Added pinia testing and coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,4 +2,14 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   // any custom Vitest config you require
+  test: {
+    environment: 'happy-dom',
+    alias: {
+      '@/': new URL('.', import.meta.url).pathname
+    },
+    coverage: {
+      provider: 'v8',
+      enabled: true
+    },
+  }
 })
