fad4f5cf4ccf44086f0d4a06860151cf7d73b51a
Marius Roggenbuck <roggenbuckmarius@gmail.com>
2024-03-28 06:19:37 +0100

    Revert "Added jsdoc with eslint"
    This reverts commit b7f0ecb3245737c0035792dbc9f98d0f00b4d85d.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,45 +5,4 @@ module.exports = {
     parser: "@typescript-eslint/parser",
   },
   extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
-  plugins: ["jsdoc"],
-  extends: ["plugin:jsdoc/recommended-typescript-error"],
-  rules: {
-    "jsdoc/require-jsdoc": [
-      "error",
-      {
-        contexts: [
-          "ClassDeclaration",
-          "ClassProperty",
-          "FunctionDeclaration",
-          "MethodDefinition",
-          "TSInterfaceDeclaration",
-        ],
-      },
-    ],
-    "jsdoc/require-description": [
-      "error",
-      {
-        contexts: [
-          "ClassDeclaration",
-          "ClassProperty",
-          "FunctionDeclaration",
-          "MethodDefinition",
-          "TSInterfaceDeclaration",
-        ],
-      },
-    ],
-    "jsdoc/require-returns": [
-      "error",
-      {
-        contexts: ["FunctionDeclaration", "MethodDefinition"],
-      },
-    ],
-    "jsdoc/require-returns-check": ["error"],
-    "jsdoc/require-returns-description": [
-      "error",
-      {
-        contexts: ["FunctionDeclaration", "MethodDefinition"],
-      },
-    ],
-  },
 };
